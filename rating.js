6623d04f8ff52d2374a85562d0af3d9a3416b94c
Sujithrakumaran <127183835+Sujithrakumaran@users.noreply.github.com>
2023-07-25 12:38:48 +0530

    Add files via upload

---
+++ rating.js
@@ -0,0 +1,40 @@
+function cryfunc(){
+   document.getElementById("star1").src="goldstar.png";
+   document.getElementById("cry").style.transform="translateX(290px)";
+   document.getElementById("cry1").style.transform="translateX(290px)";  
+   document.getElementById("cry").style.opacity="10";
+   document.getElementById("cry1").style.opacity="10";
+   
+}
+function sadfunc(){
+   document.getElementById("star2").src="goldstar.png";
+   document.getElementById("sad").style.transform="translateX(140px)";
+   document.getElementById("sad1").style.transform="translateX(140px)";  
+   document.getElementById("sad").style.opacity="10";
+   document.getElementById("sad1").style.opacity="10";
+   
+}
+function invfacefunc(){
+   document.getElementById("star3").src="goldstar.png";
+   document.getElementById("invface").style.transform="translateX(-5px)";
+   document.getElementById("invface1").style.transform="translateX(-5px)";  
+   document.getElementById("invface").style.opacity="10";
+   document.getElementById("invface1").style.opacity="10";
+
+}
+function happyfunc(){
+   document.getElementById("star4").src="goldstar.png";
+   document.getElementById("happy").style.transform="translateX(-155px)";
+   document.getElementById("happy1").style.transform="translateX(-155px)";  
+   document.getElementById("happy").style.opacity="10";
+   document.getElementById("happy1").style.opacity="10";
+
+}
+function lovefunc(){
+   document.getElementById("star5").src="goldstar.png";
+   document.getElementById("love").style.transform="translateX(-305px)";
+   document.getElementById("love1").style.transform="translateX(-305px)";  
+   document.getElementById("love").style.opacity="10";
+   document.getElementById("love1").style.opacity="10";
+
+}
